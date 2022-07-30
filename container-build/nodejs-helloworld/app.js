2d262f9ec9ebfefa67f65cdf2d3109d9d959f2ce
root <root@workstation.lab.example.com>
2022-07-30 12:52:03 -0400

    UPD

---
+++ container-build/nodejs-helloworld/app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!\n');
+});
+
+app.listen(9000, function () {
+  console.log('Example app listening on port 8080!');
+});
+
